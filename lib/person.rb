dacea06a0c31363f52249046594ec08851bc5373
Vanna Winland <vgwinland@gmail.com>
2019-09-23 20:03:17 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -11,4 +11,15 @@ class Person
     @name = new_name
   end
   
+  def job=(persons_job)
+    @job = persons_job
+  end
+  
+  def job
+    @job 
+  end
+  
+  def job=(new_job)
+    @job = new_job
+  end
 end
